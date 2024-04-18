defbdd17389922827b63e2dc6e60104f170ea36b
Eungis <eungizoa@gmail.com>
2024-04-18 18:37:54 +0900

    [fix] debug multi-turn message history disappearance issue

--- streamlit/demo/chat.py
+++ streamlit/demo/chat.py
@@ -2,8 +2,8 @@ import time
 import streamlit as st
 
 
-def clear_memory():
-    st.session_state.messages = []
+def clear_memory(message_key: str):
+    st.session_state[message_key] = []
 
 
 def response_generator():
@@ -19,28 +19,37 @@ def response_generator():
 def single_document_qa(document: dict):
     with st.popover("Multiturn QA", use_container_width=True):
         st.markdown(f"**{document['title']}**")
-        if clear := st.button("Clear Memory", key=f"{document['doc_key']}-clear-btn", on_click=clear_memory):
+        message_key = f"message-{document['doc_key']}"
+
+        if clear := st.button(
+            "Clear Memory", key=f"{document['doc_key']}-clear-btn", on_click=clear_memory, args=(message_key,)
+        ):
             st.rerun()
 
-        # Initialize chat history
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
+        prompt = st.chat_input("Your question ... ", key=f"{document['doc_key']}-input-btn")
 
-        if prompt := st.chat_input("Your question ... ", key=f"{document['doc_key']}-input-btn"):
-            # Display chat messages from history on app rerun
-            for message in st.session_state.messages:
+        # initialize chat history
+        if message_key not in st.session_state:
+            st.session_state[message_key] = []
+        else:
+            # display chat messages from history
+            for message in st.session_state[message_key]:
                 with st.chat_message(message["role"]):
                     st.markdown(message["content"])
 
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            # Display user message in chat message container
+        if prompt:
+            # add user message to chat history
+            messages = st.session_state[message_key]
+            messages.append({"role": "user", "content": prompt})
+
+            # display user message in chat message container
             with st.chat_message("user"):
                 st.markdown(prompt)
 
-            # Display assistant response in chat message container
+            # display assistant response in chat message container
             with st.chat_message("assistant"):
                 response = st.write_stream(response_generator())
 
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response})
+            # add assistant response to chat history
+            messages = st.session_state[message_key]
+            messages.append({"role": "assistant", "content": response})
